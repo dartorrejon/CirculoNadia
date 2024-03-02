7e66589771ff6d03a257e9791b1558516cae85b2
Dario Rodrigo Torrejon <dartorrejon432@outlook.com>
2024-03-02 19:36:29 -0300

    Agregado cirtulo de tareas

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Circulo from './componentes/Circulo/Circulo.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <section className='hero'>
+
+    <h1>El oficio de un Estudiante</h1>
+    <Circulo/>
+    </section>
+      
+  )
+}
+
+export default App
---
+++ src/componentes/Circulo/Circulo.jsx
@@ -0,0 +1,24 @@
+import './Circulo.css'
+const Circulo = () => {
+    return (
+        <>
+            <div className="circuloPadre">
+                <div className='linea linea1'></div>
+                <div className='linea linea2'></div>
+                <div className='linea linea3'></div>
+                <div className='linea linea4'></div>
+                <div className="division division10"></div>
+                <div className="division division9"></div>
+                <div className="division division8"></div>
+                <div className="division division7"></div>
+                <div className="division division6"></div>
+                <div className="division division5"></div>
+                <div className="division division4"></div>
+                <div className="division division3"></div>
+                <div className="division division2"></div>
+            </div>
+        </>
+    ) 
+}
+
+export default Circulo
